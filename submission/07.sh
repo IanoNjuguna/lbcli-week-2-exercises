b0cc851289dce6e066edb42182d94fef9434be52
IanoNjuguna <inc3099@gmail.com>
2026-07-02 17:45:28 +0300

    create raw tx

--- submission/07.sh
+++ submission/07.sh
@@ -1,3 +1,25 @@
+#!/usr/bin/bash
 # Create a raw transaction with an amount of 20,000,000 satoshis to this address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP 
 # Use the UTXOs from the transaction below
-# raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+
+RECIPIENT_ADDRESS="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+TX_ID=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.txid')
+VOUT_INPUT=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.vin[0].vout')
+
+# Get total value of the input UTXO to calculate change
+INPUT_VAL=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r '.vout[0].value')
+
+# Build with clean JSON
+INPUTS=$(jq -n --arg txid "$TX_ID" --argjson vout "$VOUT_INPUT" '[{txid: $txid, vout: $vout}]')
+OUTPUTS=$(jq -n --arg addr "$RECIPIENT_ADDRESS" --argjson amt 0.2 '{($addr): $amt}')
+
+# Create unsigned raw tx
+RAW_HEX=$(bitcoin-cli -regtest createrawtransaction "$INPUTS" "$OUTPUTS")
+
+# Sign tx
+SIGNED_DATA=$(bitcoin-cli -regtest signrawtransactionwithwallet "$RAW_HEX")
+
+# Broadcast to the network
+SIGNED_HEX=$(bitcoin-cli -regtest signrawtransactionwithwallet "$RAW_HEX" | jq -r '.hex')
+bitcoin-cli -regtest sendrawtransaction "$SIGNED_HEX"
