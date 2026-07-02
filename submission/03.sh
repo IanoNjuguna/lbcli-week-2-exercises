faad87ca1796caa604caf29d27d72999e622ec0f
IanoNjuguna <inc3099@gmail.com>
2026-07-02 16:49:30 +0300

    create a segwit address and add funds to it

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/bash
 # Create a SegWit address.
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 # Add funds to the address.
+FUND=$(bitcoin-cli -regtest generatetoaddress 101 "$ADDRESS")
 # Return only the Address
+echo "$ADDRESS"
+
